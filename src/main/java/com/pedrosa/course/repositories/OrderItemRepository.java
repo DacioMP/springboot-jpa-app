58d2cbdc405038bf2e3b11aaf9922778bbab3faa
Matheus Pedrosa <matheuspedrosa.contato@gmail.com>
2024-06-28 14:40:55 -0400

    OrdemItem, many-to-many association with extra attributes - part 2

---
+++ src/main/java/com/pedrosa/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.pedrosa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedrosa.course.entities.OrderItem;
+import com.pedrosa.course.entities.pk.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
